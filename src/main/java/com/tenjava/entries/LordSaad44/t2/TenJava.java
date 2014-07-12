04f3488ed8ab49843e8fe8a1247d692559407906
Saad <saadodi44@gmail.com>
2014-07-12 12:58:58 +0300

    Added new creeper drop called "Creeper Stone". Secretly is redstone. Added a right click event with that creeper stone to place it when right clicked on a block to set a redstone wire.

--- src/main/java/com/tenjava/entries/LordSaad44/t2/TenJava.java
+++ src/main/java/com/tenjava/entries/LordSaad44/t2/TenJava.java
@@ -1,6 +1,24 @@
 package com.tenjava.entries.LordSaad44.t2;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.BlockFace;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Creeper;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class TenJava extends JavaPlugin {
 
@@ -19,4 +37,51 @@ public class TenJava extends JavaPlugin {
         System.out.println("The plugin has been disabled successfully");
         System.out.println("==============");
     }
+
+    @EventHandler
+    public void onCreeperKill(EntityDeathEvent event) {
+        if (event.getEntity() instanceof Creeper) {
+            Creeper creeper = (Creeper) event.getEntity();
+            if (creeper.getKiller() instanceof Player) {
+                Player p = creeper.getKiller();
+                Random rnd = new Random();
+                int randomnumber = rnd.nextInt(2);
+                if (randomnumber == 2) {
+                    Location loc = creeper.getLocation();
+                    ItemStack item = new ItemStack(Material.SULPHUR);
+                    ItemMeta meta = item.getItemMeta();
+                    meta.addEnchant(Enchantment.LURE, 1, true);
+                    meta.setDisplayName(ChatColor.DARK_BLUE + "" + ChatColor.BOLD + "Creeper Stone");
+                    item.setItemMeta(meta);
+                    List<String> lore = new ArrayList<String>();
+                    lore.add(ChatColor.YELLOW + "Use me like redstone or use me for fuel.");
+                    Bukkit.getServer().getWorld(creeper.getWorld().getUID()).dropItemNaturally(loc, item);
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onRightClickBlock(PlayerInteractEvent event) {
+        if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            if (event.getClickedBlock().getType().isSolid()) {
+                if (!event.getClickedBlock().getType().isTransparent()) {
+                    if (event.getPlayer().getItemInHand().getType() == Material.SULPHUR) {
+                        if (event.getPlayer().getItemInHand().hasItemMeta()) {
+                            if (event.getPlayer().getItemInHand().getItemMeta().hasEnchant(Enchantment.LURE)) {
+                                int x = event.getClickedBlock().getX();
+                                int y = event.getClickedBlock().getY();
+                                int z = event.getClickedBlock().getZ();
+                                Location loc = new Location(Bukkit.getWorld(event.getPlayer().getWorld().getName()), x, y, z);
+                                if (Bukkit.getWorld(event.getPlayer().getWorld().getName()).getBlockAt(loc).getRelative(BlockFace.UP).getType() == Material.AIR) {
+                                    Bukkit.getWorld(loc.getWorld().getName()).getBlockAt(loc).getRelative(BlockFace.UP).setType(Material.REDSTONE);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
+
