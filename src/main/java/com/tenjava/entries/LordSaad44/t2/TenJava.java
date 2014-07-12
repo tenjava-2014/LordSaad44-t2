76a53168e4227e2e166e5faa5fe9b5f3bc9bb41b
Saad <saadodi44@gmail.com>
2014-07-12 12:16:15 +0300

    Added onEnable and onDisable

--- src/main/java/com/tenjava/entries/LordSaad44/t2/TenJava.java
+++ src/main/java/com/tenjava/entries/LordSaad44/t2/TenJava.java
@@ -2,4 +2,21 @@ package com.tenjava.entries.LordSaad44.t2;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+public class TenJava extends JavaPlugin {
+
+    public void onEnable() {
+        System.out.println("==============");
+        System.out.println("Creeper Energy by LordSaad44");
+        System.out.println("");
+        System.out.println("The plugin has been enabled successfully");
+        System.out.println("==============");
+    }
+
+    public void onDisable() {
+        System.out.println("==============");
+        System.out.println("Creeper Energy by LordSaad44");
+        System.out.println("");
+        System.out.println("The plugin has been disabled successfully");
+        System.out.println("==============");
+    }
+}
