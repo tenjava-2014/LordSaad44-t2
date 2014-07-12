8880a660de26c10dea3c7c6ce52345e7c2724591
Saad <saadodi44@gmail.com>
2014-07-12 14:19:55 +0300

    Changed creeper stone display name from bolded blue to regular dark red. Unnefed furnace cook time to 1000.

--- src/main/java/com/tenjava/entries/LordSaad44/t2/TenJava.java
+++ src/main/java/com/tenjava/entries/LordSaad44/t2/TenJava.java
@@ -8,6 +8,7 @@ import org.bukkit.block.BlockFace;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Creeper;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.inventory.FurnaceBurnEvent;
@@ -18,14 +19,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
-public class TenJava extends JavaPlugin {
+public class TenJava extends JavaPlugin implements Listener {
 
     public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
         System.out.println("==============");
         System.out.println("Creeper Energy by LordSaad44");
-        System.out.println("");
+        System.out.println(" ");
         System.out.println("The plugin has been enabled successfully");
         System.out.println("==============");
     }
@@ -33,7 +34,7 @@ public class TenJava extends JavaPlugin {
     public void onDisable() {
         System.out.println("==============");
         System.out.println("Creeper Energy by LordSaad44");
-        System.out.println("");
+        System.out.println(" ");
         System.out.println("The plugin has been disabled successfully");
         System.out.println("==============");
     }
@@ -43,20 +44,16 @@ public class TenJava extends JavaPlugin {
         if (event.getEntity() instanceof Creeper) {
             Creeper creeper = (Creeper) event.getEntity();
             if (creeper.getKiller() != null) {
-                Random rnd = new Random();
-                int randomnumber = rnd.nextInt(2);
-                if (randomnumber == 2) {
-                    Location loc = creeper.getLocation();
-                    ItemStack item = new ItemStack(Material.SULPHUR);
-                    ItemMeta meta = item.getItemMeta();
-                    meta.addEnchant(Enchantment.LURE, 1, true);
-                    meta.setDisplayName(ChatColor.DARK_BLUE + "" + ChatColor.BOLD + "Creeper Stone");
-                    List<String> lore = new ArrayList<String>();
-                    lore.add(ChatColor.YELLOW + "Use me like redstone or use me for fuel. I'm good energy.");
-                    meta.setLore(lore);
-                    item.setItemMeta(meta);
-                    Bukkit.getServer().getWorld(creeper.getWorld().getUID()).dropItemNaturally(loc, item);
-                }
+                Location loc = creeper.getLocation();
+                ItemStack item = new ItemStack(Material.SULPHUR);
+                ItemMeta meta = item.getItemMeta();
+                meta.addEnchant(Enchantment.LURE, 1, true);
+                meta.setDisplayName(ChatColor.DARK_RED + "Creeper Stone");
+                List<String> lore = new ArrayList<String>();
+                lore.add(ChatColor.YELLOW + "Use me like redstone or use me for fuel. I'm good energy.");
+                meta.setLore(lore);
+                item.setItemMeta(meta);
+                Bukkit.getServer().getWorld(creeper.getWorld().getUID()).dropItemNaturally(loc, item);
             }
         }
     }
@@ -89,7 +86,7 @@ public class TenJava extends JavaPlugin {
         if (event.getFuel().getType() == Material.SULPHUR) {
             if (event.getFuel().hasItemMeta()) {
                 if (event.getFuel().getItemMeta().hasEnchant(Enchantment.LURE)) {
-                    event.setBurnTime(100);
+                    event.setBurnTime(1000);
                     event.setBurning(true);
                 }
             }
